30cc5696e00216a557d9d12a0f8b71f93f2ce07b
Griffen Fargo <ghfargo@gmail.com>
2020-03-26 15:10:29 -0400

    Remove private keys from repo in favor of env. variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,18 @@ module.exports = {
       options: {
           spreadsheetId: '1xSZHv2emLxkBFViORfG42SYpWtwM-ZbwrQ7CfHX5WXQ',
           worksheetTitle: 'Stats',
-          credentials: require('./src/config/sov-covid-19-stats-64efa14b0b93.json')
+          credentials: {
+            "type": "service_account",
+            "project_id": process.env.GSERVICE_PROJECT_ID,
+            "private_key_id": process.env.GSERVICE_KEY_ID,
+            "private_key": process.env.GSERVICE_PRIVATE_KEY,
+            "client_email":  process.env.GSERVICE_CLIENT_EMAIL,
+            "client_id": process.env.GSERVICE_CLIENT_EMAIL,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url":  process.env.GSERVICE_CLIENT_CERT_URL
+          }
       }
     },
     {
