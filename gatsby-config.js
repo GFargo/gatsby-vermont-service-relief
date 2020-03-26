e023391963f55ca31f79a096564f906890aefc1c
Griffen Fargo <ghfargo@gmail.com>
2020-03-26 15:29:38 -0400

    Munge JWT token formatting
    Converts JS obj into JSON string

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,19 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`
 });
 
+const googleServiceJWT = {
+  "type": "service_account",
+  "project_id": process.env.GSERVICE_PROJECT_ID,
+  "private_key_id": process.env.GSERVICE_KEY_ID,
+  "private_key": process.env.GSERVICE_PRIVATE_KEY,
+  "client_email": process.env.GSERVICE_CLIENT_EMAIL,
+  "client_id": process.env.GSERVICE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url":  process.env.GSERVICE_CLIENT_CERT_URL
+};
+
 module.exports = {
   siteMetadata: {
     title: process.env.COMMUNITY ? `${process.env.COMMUNITY} Service Relief` : `{COMMUNITY} Service Relief`,
@@ -39,18 +52,7 @@ module.exports = {
       options: {
         spreadsheetId: '1xSZHv2emLxkBFViORfG42SYpWtwM-ZbwrQ7CfHX5WXQ',
         worksheetTitle: 'Stats',
-        credentials: {
-          "type": "service_account",
-          "project_id": process.env.GSERVICE_PROJECT_ID,
-          "private_key_id": process.env.GSERVICE_KEY_ID,
-          "private_key": process.env.GSERVICE_PRIVATE_KEY,
-          "client_email": process.env.GSERVICE_CLIENT_EMAIL,
-          "client_id": process.env.GSERVICE_CLIENT_ID,
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://oauth2.googleapis.com/token",
-          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-          "client_x509_cert_url":  process.env.GSERVICE_CLIENT_CERT_URL
-        }
+        credentials: JSON.stringify(googleServiceJWT),
       }
     },
     {
