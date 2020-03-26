200f979babf7045899985f2b984923a395b6639a
Griffen Fargo <ghfargo@gmail.com>
2020-03-26 15:31:36 -0400

    Removed blank space & JSON stringify

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ const googleServiceJWT = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url":  process.env.GSERVICE_CLIENT_CERT_URL
+  "client_x509_cert_url": process.env.GSERVICE_CLIENT_CERT_URL
 };
 
 module.exports = {
@@ -52,7 +52,7 @@ module.exports = {
       options: {
         spreadsheetId: '1xSZHv2emLxkBFViORfG42SYpWtwM-ZbwrQ7CfHX5WXQ',
         worksheetTitle: 'Stats',
-        credentials: JSON.stringify(googleServiceJWT),
+        credentials: googleServiceJWT,
       }
     },
     {
